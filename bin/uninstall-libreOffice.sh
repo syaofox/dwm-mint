a1baf3af080437749c7662a788f05d62442c3034
syaofox <syaofox@gmail.com>
2026-04-27 11:09:45 -0400

    1

--- bin/uninstall-libreOffice.sh
+++ bin/uninstall-libreOffice.sh
@@ -10,14 +10,11 @@ echo "=========================================="
 echo "--- 正在移除软件包 ---"
 sudo apt-get purge -y libreoffice*
 
-# 2. 移除孤立的依赖包
-echo "--- 正在清理无用的依赖 ---"
-sudo apt-get autoremove -y
 
-# 3. 清理下载的包缓存
+# 2. 清理下载的包缓存
 sudo apt-get autoclean
 
-# 4. 删除用户目录下的个人配置 (可选)
+# 3. 删除用户目录下的个人配置 (可选)
 # 这一步会删除所有用户的 LibreOffice 配置（如自定义快捷键、模板等）
 echo "--- 正在清理用户配置文件 ---"
 sudo rm -rf /home/*/.config/libreoffice
