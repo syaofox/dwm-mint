735a47f591eb85cb3e5e62e97cef1ff402ba9d86
syaofox <syaofox@gmail.com>
2026-04-30 17:41:12 +0800

    fzf 自动确认

--- setup/install-fzf.sh
+++ setup/install-fzf.sh
@@ -16,7 +16,7 @@ if ! git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; then
 fi
 
 log_info "Running fzf install script..."
-if ! ~/.fzf/install; then
+if ! ~/.fzf/install --all --no-update-rc; then
     log_error "Failed to run fzf install script"
     exit 1
 fi
