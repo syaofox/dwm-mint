d62c41caece3f2b4d780de0071cfd324f87baa56
syaofox <syaofox@gmail.com>
2026-05-08 19:02:41 +0800

    123

--- setup/install-directories.sh
+++ setup/install-directories.sh
@@ -4,9 +4,12 @@ source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
 log_step "Creating common directories..."
 
-dirs=(
+user_dirs=(
     "$HOME/tmp"
     "$HOME/project"
+)
+
+mnt_dirs=(
     "/mnt/dnas/backup"
     "/mnt/dnas/data"
     "/mnt/dnas/download"
@@ -14,9 +17,19 @@ dirs=(
     "/mnt/xiaoxin/data"
 )
 
-for dir in "${dirs[@]}"; do
+for dir in "${user_dirs[@]}"; do
     if [ ! -d "$dir" ]; then
         mkdir -p "$dir"
+        log_info "Created: $dir"
+    else
+        log_info "Already exists: $dir"
+    fi
+done
+
+for dir in "${mnt_dirs[@]}"; do
+    if [ ! -d "$dir" ]; then
+        sudo mkdir -p "$dir"
+        sudo chown "$USER:$USER" "$dir"
         log_info "Created: $dir"
     else
         log_info "Already exists: $dir"
